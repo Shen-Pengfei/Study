d1aab50383fe3257f8d05775d48069bbc3982a28
shenpf <shen_peng_fei@126.com>
2019-08-13 20:36:50 +0800

    1、修复一些bug 2、增加自动检索文件中网址

--- Python/SiteMonitoring/setup.py
+++ Python/SiteMonitoring/setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+# __version__ = "$Revision$"
 
 import urllib.request
 import smtplib
@@ -8,28 +9,30 @@ from email.utils import formataddr
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+# 发送邮件
 def E_Mail(receive,body,sender='xxxx@xx.xxx',password='xxxxxx',mailserver='smtp.exmail.qq.com',port='25'):
     """
     :param sender: 发件人邮箱
     :param password: 邮箱密码
     :param receive: 收件人邮箱
+    :param body: 邮件内容
     :param mailserver: 发送邮件服务器：smtp.域名
     :param port: 发送邮件服务器端口号
     :return:
     """
 
-    sub = 'Python3 test'    # 主题
+    sub = 'Python3 test'  # 主题
     try:
         msg = MIMEMultipart('related')
-        msg['From'] = formataddr(["Web Monitoring ", sender])    # 发件人邮箱昵称、发件人邮箱账号
+        msg['From'] = formataddr(["Web Monitoring ", sender])  # 发件人邮箱昵称、发件人邮箱账号
         # msg['To'] = formataddr(["receiver", receive])   # 收件人邮箱昵称、发件人邮箱账号
         msg['Subject'] = sub
 
         text = MIMEText(body, 'html', 'utf-8')
         msg.attach(text)
 
-        smtpObj = smtplib.SMTP(mailserver, port)    # 指定发件人邮箱中的SMTP服务器，端口是25
-        smtpObj.login(sender, password)     # 登陆发件人邮箱
+        smtpObj = smtplib.SMTP(mailserver, port)  # 指定发件人邮箱中的SMTP服务器，端口是25
+        smtpObj.login(sender, password)  # 登陆发件人邮箱
         smtpObj.sendmail(sender, receive, msg.as_string())  # 发送邮件。发件人邮箱账号、收件人邮箱账号、发送邮件
         smtpObj.quit()
         print('success')
@@ -37,80 +40,65 @@ def E_Mail(receive,body,sender='xxxx@xx.xxx',password='xxxxxx',mailserver='smtp.
         print(e)
 
 
-def html(url, Status_code):
-    haed = """
-    <!DOCTYPE html><html>
+# HTML模板
+def template_html():
+    haed = \
+        """
+    <html>
     <head>
         <meta charset='utf-8'>
         <meta http-equiv='X-UA-Compatible' content='IE=edge'>
         <title>web监控</title>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
-    </head>
-    """
-    body = """
-    <body>
+    </head>"""
+    body = """<body>
         <table width="400" align="center">
             <tr>
                 <th>网址</th>
                 <th>状态码</th>
             </tr>
-            <tr>
-                <th>"""
-    body1 = """</th><th>"""
-    body2 = """</th>
-            </tr>
-        </table>
+    """
+    bodyInfo = ''
+    for i in range(len(site_list)):
+        bodyInfo += '<tr>'
+        bodyInfo += '<th>' + site_list[i] + '</th>'
+        bodyInfo += '<th>' + str(code_list[i]) + '</th>'
+        bodyInfo += '</tr>'
+
+    html = haed + body + bodyInfo
+    html += """
+    </table>
     </body>
     </html>
     """
-    while True:
-        url + Status_code
-    page = haed+body
-    print(page)
+    print('HHH', html)
+    return html
 
 
+# 获取url code值
 def check_site(url_list):
     """
-    :param url: 传入url列表
+    :param url_list: 传入url列表
     :return:
     """
     sites = open(url_list)
-
-    site_list = []
     print(type(site_list))
-    code_list = []
 
     for line in sites:
         # 截取网址
         site = line.strip()
         site_list.append(site)
-        # print(site)
         try:
+            # 获取网页当前状态码
             response = urllib.request.urlopen(site)
             code = response.status
             code_list.append(code)
-
-            # print(code)
-            E_Mail("xxxx@xx.xxx", str(code))
-            # print(code)
-            # html(site, code)
-
-            # E_Mail("shenpf@etlchina.net", h)
         except Exception as e:
             print(e)
+    E_Mail("123456@qq.com", template_html())
 
-    # count = 0
-    # while True:
-    #     if site_list[count] == site_list[-1]:
-    #         # print(site_list[count])
-    #         print("b")
-    #         break
-    #     else:
-    #         print(count)
-    #         print(site_list[count])
-    #
-    #         count = count + 1
 
 if __name__ == '__main__':
+    site_list = []
+    code_list = []
     check_site('domain.dic')
-
